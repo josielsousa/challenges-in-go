0cc189f1be5c683b6a14370f3c4d4e7dd95c2236
Josiel Sousa <josiel.psousa@gmail.com>
2022-08-26 20:41:29 -0300

    chore: leetcode first pratice

---
+++ leetcode/roman_to_int.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func romanToInt(s string) int {
+	romans := map[string]int{
+		"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,
+	}
+
+	sufix := map[string]string{
+		"I": "VX", "X": "LC", "C": "DM",
+	}
+
+	var sum int
+
+	for i := len(s) - 1; i >= 0; i-- {
+		key := string(s[i])
+
+		if svdataset, ok := sufix[key]; ok && i+1 < len(s) {
+			if svdataset[0] == s[i+1] || svdataset[1] == s[i+1] {
+				sum -= romans[key]
+				continue
+			}
+		}
+
+		sum += romans[key]
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(romanToInt("MCMXCIV")) // 1994
+
+	fmt.Println(romanToInt("IV"))	// 4
+	fmt.Println(romanToInt("IX"))   // 9
+
+	fmt.Println(romanToInt("LVIII")) // 58
+	fmt.Println(romanToInt("XXVII")) // 27
+}
